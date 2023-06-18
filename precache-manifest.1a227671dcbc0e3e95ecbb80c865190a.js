8f6b14a4a0b72c3b7e3c9f62ed9582d39b893f8f
Vishal Srivastava <75941661+Dark8203@users.noreply.github.com>
2023-06-19 00:19:30 +0530

    Updates

---
+++ precache-manifest.1a227671dcbc0e3e95ecbb80c865190a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47f8f0f901ebc5098fbac0df0bb960b0",
+    "url": "/Calculator_React/index.html"
+  },
+  {
+    "revision": "8c9b6b779e6626b54e9d",
+    "url": "/Calculator_React/static/css/main.489b2cb9.chunk.css"
+  },
+  {
+    "revision": "3c8d2e142aa2ea988c0e",
+    "url": "/Calculator_React/static/js/2.772c2dd3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Calculator_React/static/js/2.772c2dd3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c9b6b779e6626b54e9d",
+    "url": "/Calculator_React/static/js/main.592b6314.chunk.js"
+  },
+  {
+    "revision": "ec3e2f86f893d0494669",
+    "url": "/Calculator_React/static/js/runtime-main.96c43a2d.js"
+  }
+]);
